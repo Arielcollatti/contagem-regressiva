e48c15f863139ab81675d9da1cfe305631b3023f
arielcollatti <colatti2017@gmail.com>
2023-03-08 14:02:22 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("contagem-regressive");
+Console.WriteLine("");
+ 
+ Console.ReadKey();
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
+Console.ReadKey();
